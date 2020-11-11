96f54bfeaa2605570b6f8b63a5ed14066b2520a1
Dom Del Nano <ddelnano@gmail.com>
2020-11-11 02:40:35 +0000

    Add pool client support, with tests

---
+++ client/pool.go
@@ -0,0 +1,123 @@
+package client
+
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+type Pool struct {
+	Id       string `mikrotik:".id"`
+	Name     string
+	Ranges   string `mikrotik:"ranges"`
+	Comment  string
+	NextPool string `mikrotik"next-pool"`
+}
+
+func (client Mikrotik) AddPool(name string, ranges string, comment string, nextpool string) (*Pool, error) {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return nil, err
+	}
+	cmd := strings.Split(fmt.Sprintf("/ip/pool/add =name=%s =ranges=%s =comment=%s =next-pool=%s", name, ranges, comment, nextpool), " ")
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	r, err := c.RunArgs(cmd)
+
+	log.Printf("[DEBUG] Pool creation response: `%v`", r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	id := r.Done.Map["ret"]
+
+	return client.FindPool(id)
+}
+
+func (client Mikrotik) ListPools() ([]Pool, error) {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return nil, err
+	}
+	cmd := []string{"/ip/pool/print"}
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	r, err := c.RunArgs(cmd)
+
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("[DEBUG] Found pools: %v", r)
+
+	pools := []Pool{}
+
+	err = Unmarshal(*r, &pools)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pools, nil
+}
+
+func (client Mikrotik) FindPool(id string) (*Pool, error) {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return nil, err
+	}
+	cmd := strings.Split(fmt.Sprintf("/ip/pool/print ?.id=%s", id), " ")
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	r, err := c.RunArgs(cmd)
+
+	log.Printf("[DEBUG] Pool response: %v", r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	pool := Pool{}
+	err = Unmarshal(*r, &pool)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if pool.Id == "" {
+		return nil, NewNotFound(fmt.Sprintf("pool `%s` not found", id))
+	}
+
+	return &pool, nil
+}
+
+func (client Mikrotik) UpdatePool(id, ranges string, comment string, nextpool string) (*Pool, error) {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	cmd := strings.Split(fmt.Sprintf("/ip/pool/set =.id=%s =ranges=%s =comment=%s =next-pool=%s", id, ranges, comment, nextpool), " ")
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	_, err = c.RunArgs(cmd)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client.FindPool(id)
+}
+
+func (client Mikrotik) DeletePool(id string) error {
+	c, err := client.getMikrotikClient()
+
+	if err != nil {
+		return err
+	}
+
+	cmd := strings.Split(fmt.Sprintf("/ip/pool/remove =.id=%s", id), " ")
+	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
+	_, err = c.RunArgs(cmd)
+	return err
+}
---
+++ client/pool_test.go
@@ -0,0 +1,82 @@
+package client
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestAddPoolAndDeletePool(t *testing.T) {
+	c := NewClient(GetConfigFromEnv())
+
+	name := "testacc"
+	ranges := "172.16.0.1-172.16.0.8,172.16.0.10"
+	comment := "terraform-acc-test-pool"
+	nextpool := "none"
+	expectedPool := &Pool{
+		Name:     name,
+		Ranges:   ranges,
+		Comment:  comment,
+		NextPool: nextpool,
+	}
+	pool, err := c.AddPool(
+		name,
+		ranges,
+		comment,
+		nextpool,
+	)
+
+	fmt.Sprintf("expected pool:  %v", expectedPool)
+	fmt.Sprintf("created pool:  %v", pool)
+	if err != nil {
+		t.Errorf("Error creating a pool with: %v", err)
+	}
+
+	if len(pool.Id) < 1 {
+		t.Errorf("The created pool does not have an Id: %v", pool)
+	}
+
+	if strings.Compare(pool.Name, expectedPool.Name) != 0 {
+		t.Errorf("The pool Name fields do not match. actual: %v expected: %v", pool.Name, expectedPool.Name)
+	}
+
+	if strings.Compare(pool.Ranges, expectedPool.Ranges) != 0 {
+		t.Errorf("The pool Ranges fields do not match. actual: %v expected: %v", pool.Ranges, expectedPool.Ranges)
+	}
+
+	if strings.Compare(pool.Comment, expectedPool.Comment) != 0 {
+		t.Errorf("The pool Comment fields do not match. actual: %v expected: %v", pool.Comment, expectedPool.Comment)
+	}
+	if nextpool != "none" && strings.Compare(pool.NextPool, expectedPool.NextPool) != 0 {
+		t.Errorf("The pool NextPool fields do not match. actual: %v expected: %v", pool.NextPool, expectedPool.NextPool)
+	}
+
+	foundPool, err := c.FindPool(pool.Id)
+
+	if err != nil {
+		t.Errorf("Error getting pool with: %v", err)
+	}
+
+	if !reflect.DeepEqual(pool, foundPool) {
+		t.Errorf("Created pool and found pool do not match. actual: %v expected: %v", foundPool, pool)
+	}
+
+	err = c.DeletePool(pool.Id)
+
+	if err != nil {
+		t.Errorf("Error deleting pool with: %v", err)
+	}
+}
+
+func TestFindPool_forNonExistingPool(t *testing.T) {
+	c := NewClient(GetConfigFromEnv())
+
+	poolId := "Invalid id"
+	_, err := c.FindPool(poolId)
+
+	expectedErrStr := fmt.Sprintf("pool `%s` not found", poolId)
+	if err == nil || err.Error() != expectedErrStr {
+		t.Errorf("client should have received error indicating the following pool with id `%s` was not found. Instead error was nil", poolId)
+	}
+}
