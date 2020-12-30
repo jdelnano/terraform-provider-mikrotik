02c8a3d9f9c63441977fb89b4956fd8abc5be395
jdelnano <jdelnano@gmail.com>
2020-12-29 21:59:00 -0500

    Fix typos (causing delete issue) and make golint happy

--- client/bgp_instance.go
+++ client/bgp_instance.go
@@ -6,8 +6,9 @@ import (
 	"strings"
 )
 
+// BgpInstance Mikrotik resource
 type BgpInstance struct {
-	Id                       string `mikrotik:".id"`
+	ID                       string `mikrotik:".id"`
 	Name                     string
 	As                       int    `mikroktik:"as"`
 	ClientToClientReflection bool   `mikroktik:"client-to-client-reflection"`
@@ -23,33 +24,34 @@ type BgpInstance struct {
 	RedistributeOtherBgp  bool   `mikrotik:"redistribute-other-bgp"`
 	RedistributeRip       bool   `mikrotik:"redistribute-rip"`
 	RedistributeStatic    bool   `mikrotik:"redistribute-static"`
-	RouterId              string `mikroktik:"router-id"`
+	RouterID              string `mikroktik:"router-id"`
 	RoutingTable          string `mikroktik:"routing-table"`
-	ClusterId             string `mikroktik:"cluster-id"`
+	ClusterID             string `mikroktik:"cluster-id"`
 	Confederation         int    `mikrotik:"confederation"`
 }
 
-func (client Mikrotik) AddBgpInstance(name string, as int, clientToClientReflection bool, comment string, confederationPeers string, disabled bool, ignoreAsPathLen bool, outFilter string, redistributeConnected bool, redistributeOspf bool, redistributeOtherBgp bool, redistributeRip bool, redistributeStatic bool, routerId string, routingTable string, clusterId string, confederation int) (*BgpInstance, error) {
+// AddBgpInstance Mikrotik resource
+func (client Mikrotik) AddBgpInstance(name string, as int, clientToClientReflection bool, comment string, confederationPeers string, disabled bool, ignoreAsPathLen bool, outFilter string, redistributeConnected bool, redistributeOspf bool, redistributeOtherBgp bool, redistributeRip bool, redistributeStatic bool, routerID string, routingTable string, clusterID string, confederation int) (*BgpInstance, error) {
 	c, err := client.getMikrotikClient()
 
 	if err != nil {
 		return nil, err
 	}
 
-	cmd := strings.Split(fmt.Sprintf("/routing/bgp/instance/add =as=%d =name=%s =client-to-client-reflection=%s =comment=%s =confederation-peers=%s =disabled=%s =ignore-as-path-len=%s =out-filter=%s =redistribute-connected=%s =redistribute-ospf=%s =redistribute-other-bgp=%s =redistribute-rip=%s =redistribute-static=%s =router-id=%s =routing-table=%s", as, name, boolToMikrotikBool(clientToClientReflection), comment, confederationPeers, boolToMikrotikBool(disabled), boolToMikrotikBool(ignoreAsPathLen), outFilter, boolToMikrotikBool(redistributeConnected), boolToMikrotikBool(redistributeOspf), boolToMikrotikBool(redistributeOtherBgp), boolToMikrotikBool(redistributeRip), boolToMikrotikBool(redistributeStatic), routerId, routingTable), " ")
+	cmd := strings.Split(fmt.Sprintf("/routing/bgp/instance/add =as=%d =name=%s =client-to-client-reflection=%s =comment=%s =confederation-peers=%s =disabled=%s =ignore-as-path-len=%s =out-filter=%s =redistribute-connected=%s =redistribute-ospf=%s =redistribute-other-bgp=%s =redistribute-rip=%s =redistribute-static=%s =router-id=%s =routing-table=%s", as, name, boolToMikrotikBool(clientToClientReflection), comment, confederationPeers, boolToMikrotikBool(disabled), boolToMikrotikBool(ignoreAsPathLen), outFilter, boolToMikrotikBool(redistributeConnected), boolToMikrotikBool(redistributeOspf), boolToMikrotikBool(redistributeOtherBgp), boolToMikrotikBool(redistributeRip), boolToMikrotikBool(redistributeStatic), routerID, routingTable), " ")
 
 	// optionally append fields if they are set
 	// cannot include them empty otherwise Mikrotik has fit
 	if confederation != -1 {
 		cmd = append(cmd, fmt.Sprintf("=confederation=%d", confederation))
 	}
-	if clusterId != "" {
-		cmd = append(cmd, fmt.Sprintf("=cluster-id=%s", clusterId))
+	if clusterID != "" {
+		cmd = append(cmd, fmt.Sprintf("=cluster-id=%s", clusterID))
 	}
 
 	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
 	r, err := c.RunArgs(cmd)
-	log.Printf("[DEBUG] /routing/bpg/instance/add returned %v", r)
+	log.Printf("[DEBUG] /routing/bgp/instance/add returned %v", r)
 
 	if err != nil {
 		return nil, err
@@ -58,6 +60,7 @@ func (client Mikrotik) AddBgpInstance(name string, as int, clientToClientReflect
 	return client.FindBgpInstance(name)
 }
 
+// FindBgpInstance Mikrotik resource
 func (client Mikrotik) FindBgpInstance(name string) (*BgpInstance, error) {
 	c, err := client.getMikrotikClient()
 	if err != nil {
@@ -89,7 +92,8 @@ func (client Mikrotik) FindBgpInstance(name string) (*BgpInstance, error) {
 	return &bgpInstance, nil
 }
 
-func (client Mikrotik) UpdateBgpInstance(name string, as int, clientToClientReflection bool, comment string, confederationPeers string, disabled bool, ignoreAsPathLen bool, outFilter string, redistributeConnected bool, redistributeOspf bool, redistributeOtherBgp bool, redistributeRip bool, redistributeStatic bool, routerId string, routingTable string, clusterId string, confederation int) (*BgpInstance, error) {
+// UpdateBgpInstance Mikrotik resource
+func (client Mikrotik) UpdateBgpInstance(name string, as int, clientToClientReflection bool, comment string, confederationPeers string, disabled bool, ignoreAsPathLen bool, outFilter string, redistributeConnected bool, redistributeOspf bool, redistributeOtherBgp bool, redistributeRip bool, redistributeStatic bool, routerID string, routingTable string, clusterID string, confederation int) (*BgpInstance, error) {
 	c, err := client.getMikrotikClient()
 
 	if err != nil {
@@ -101,7 +105,7 @@ func (client Mikrotik) UpdateBgpInstance(name string, as int, clientToClientRefl
 	if err != nil {
 		return bgpInstance, err
 	}
-	cmd := strings.Split(fmt.Sprintf("/routing/bgp/instance/set =numbers=%s =as=%d =name=%s =client-to-client-reflection=%s =comment=%s =confederation-peers=%s =disabled=%s =ignore-as-path-len=%s =out-filter=%s =redistribute-connected=%s =redistribute-ospf=%s =redistribute-other-bgp=%s =redistribute-rip=%s =redistribute-static=%s =router-id=%s =routing-table=%s", bgpInstance.Id, as, name, boolToMikrotikBool(clientToClientReflection), comment, confederationPeers, boolToMikrotikBool(disabled), boolToMikrotikBool(ignoreAsPathLen), outFilter, boolToMikrotikBool(redistributeConnected), boolToMikrotikBool(redistributeOspf), boolToMikrotikBool(redistributeOtherBgp), boolToMikrotikBool(redistributeRip), boolToMikrotikBool(redistributeStatic), routerId, routingTable), " ")
+	cmd := strings.Split(fmt.Sprintf("/routing/bgp/instance/set =numbers=%s =as=%d =name=%s =client-to-client-reflection=%s =comment=%s =confederation-peers=%s =disabled=%s =ignore-as-path-len=%s =out-filter=%s =redistribute-connected=%s =redistribute-ospf=%s =redistribute-other-bgp=%s =redistribute-rip=%s =redistribute-static=%s =router-id=%s =routing-table=%s", bgpInstance.ID, as, name, boolToMikrotikBool(clientToClientReflection), comment, confederationPeers, boolToMikrotikBool(disabled), boolToMikrotikBool(ignoreAsPathLen), outFilter, boolToMikrotikBool(redistributeConnected), boolToMikrotikBool(redistributeOspf), boolToMikrotikBool(redistributeOtherBgp), boolToMikrotikBool(redistributeRip), boolToMikrotikBool(redistributeStatic), routerID, routingTable), " ")
 
 	// optionally append fields if they are set
 	// cannot include them empty otherwise Mikrotik has fit
@@ -110,8 +114,8 @@ func (client Mikrotik) UpdateBgpInstance(name string, as int, clientToClientRefl
 		cmd = append(cmd, fmt.Sprintf("=confederation=%d", confederation))
 	}
 	// TODO: `unset` doesn't work :(
-	if clusterId != "" {
-		cmd = append(cmd, fmt.Sprintf("=cluster-id=%s", clusterId))
+	if clusterID != "" {
+		cmd = append(cmd, fmt.Sprintf("=cluster-id=%s", clusterID))
 	}
 
 	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
@@ -124,6 +128,7 @@ func (client Mikrotik) UpdateBgpInstance(name string, as int, clientToClientRefl
 	return client.FindBgpInstance(name)
 }
 
+// DeleteBgpInstance Mikrotik resource
 func (client Mikrotik) DeleteBgpInstance(name string) error {
 	c, err := client.getMikrotikClient()
 
@@ -133,7 +138,7 @@ func (client Mikrotik) DeleteBgpInstance(name string) error {
 		return err
 	}
 
-	cmd := strings.Split(fmt.Sprintf("/routing/bpg/instance/remove =numbers=%s", bgpInstance.Name), " ")
+	cmd := strings.Split(fmt.Sprintf("/routing/bgp/instance/remove =numbers=%s", bgpInstance.Name), " ")
 	log.Printf("[INFO] Running the mikrotik command: `%s`", cmd)
 	r, err := c.RunArgs(cmd)
 	log.Printf("[DEBUG] Remove bgp instance via mikrotik api: %v", r)
--- client/bgp_instance_test.go
+++ client/bgp_instance_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 )
 
-var bgp_name string = "test-bgp"
+var bgpName string = "test-bgp"
 var as int = 65534
 var clientToClientReflection bool = true
-var clusterId string = "172.21.16.1"
-var noClusterId string = ""
-var bgp_comment string = ""
+var clusterID string = "172.21.16.1"
+var noClusterID string = ""
+var bgpComment string = ""
 var confederation int = 7
 var noConfederation int = -1
 var confederationPeers string = ""
@@ -22,17 +22,17 @@ var redistributeOspf bool = false
 var redistributeOtherBgp bool = false
 var redistributeRip bool = false
 var redistributeStatic bool = false
-var routerId string = "172.21.16.2"
+var routerID string = "172.21.16.2"
 var routingTable string = ""
 
 func TestAddBgpInstanceIncludingOptionalFieldsAndDeleteBgpInstance(t *testing.T) {
 	c := NewClient(GetConfigFromEnv())
 
 	expectedBgpInstance := &BgpInstance{
-		Name:                     bgp_name,
+		Name:                     bgpName,
 		As:                       as,
 		ClientToClientReflection: clientToClientReflection,
-		Comment:                  bgp_comment,
+		Comment:                  bgpComment,
 		ConfederationPeers:       confederationPeers,
 		Disabled:                 disabled,
 		IgnoreAsPathLen:          ignoreAsPathLen,
@@ -42,16 +42,16 @@ func TestAddBgpInstanceIncludingOptionalFieldsAndDeleteBgpInstance(t *testing.T)
 		RedistributeOtherBgp:     redistributeOtherBgp,
 		RedistributeRip:          redistributeRip,
 		RedistributeStatic:       redistributeStatic,
-		RouterId:                 routerId,
+		RouterID:                 routerID,
 		RoutingTable:             routingTable,
-		ClusterId:                clusterId,
+		ClusterID:                clusterID,
 		Confederation:            confederation,
 	}
 	bgpInstance, err := c.AddBgpInstance(
-		bgp_name,
+		bgpName,
 		as,
 		clientToClientReflection,
-		bgp_comment,
+		bgpComment,
 		confederationPeers,
 		disabled,
 		ignoreAsPathLen,
@@ -61,9 +61,9 @@ func TestAddBgpInstanceIncludingOptionalFieldsAndDeleteBgpInstance(t *testing.T)
 		redistributeOtherBgp,
 		redistributeRip,
 		redistributeStatic,
-		routerId,
+		routerID,
 		routingTable,
-		clusterId,
+		clusterID,
 		confederation,
 	)
 	if err != nil {
@@ -72,10 +72,10 @@ func TestAddBgpInstanceIncludingOptionalFieldsAndDeleteBgpInstance(t *testing.T)
 	fmt.Println(bgpInstance)
 	fmt.Println(expectedBgpInstance)
 	updatedBgpInstance, err := c.UpdateBgpInstance(
-		bgp_name,
+		bgpName,
 		as,
 		clientToClientReflection,
-		bgp_comment,
+		bgpComment,
 		confederationPeers,
 		disabled,
 		ignoreAsPathLen,
@@ -85,124 +85,17 @@ func TestAddBgpInstanceIncludingOptionalFieldsAndDeleteBgpInstance(t *testing.T)
 		redistributeOtherBgp,
 		redistributeRip,
 		redistributeStatic,
-		routerId,
+		routerID,
 		routingTable,
-		noClusterId,
+		noClusterID,
 		9,
 	)
 
 	fmt.Println(updatedBgpInstance)
 
-	//if len(pool.Id) < 1 {
-	//	t.Errorf("The created pool does not have an Id: %v", pool)
-	//}
+	err = c.DeleteBgpInstance(bgpInstance.Name)
 
-	//if pool.Name != expectedPool.Name {
-	//	t.Errorf("The pool Name fields do not match. actual: %v expected: %v", pool.Name, expectedPool.Name)
-	//}
-
-	//if pool.Ranges != expectedPool.Ranges {
-	//	t.Errorf("The pool Ranges fields do not match. actual: %v expected: %v", pool.Ranges, expectedPool.Ranges)
-	//}
-
-	//if pool.Comment != expectedPool.Comment {
-	//	t.Errorf("The pool Comment fields do not match. actual: %v expected: %v", pool.Comment, expectedPool.Comment)
-	//}
-
-	//foundPool, err := c.FindPool(pool.Id)
-
-	//if err != nil {
-	//	t.Errorf("Error getting pool with: %v", err)
-	//}
-
-	//if !reflect.DeepEqual(pool, foundPool) {
-	//	t.Errorf("Created pool and found pool do not match. actual: %v expected: %v", foundPool, pool)
-	//}
-
-	//err = c.DeleteBgpInstance(bgpInstance.Name)
-
-	//if err != nil {
-	//	t.Errorf("Error deleting bgp instance with: %v", err)
-	//}
+	if err != nil {
+		t.Errorf("Error deleting bgp instance with: %v", err)
+	}
 }
-
-//func TestAddBgpInstanceExcludingOptionalFieldsAndDeleteBgpInstance(t *testing.T) {
-//	c := NewClient(GetConfigFromEnv())
-//
-//	expectedBgpInstance := &BgpInstance{
-//		Name:                     bgp_name,
-//		As:                       as,
-//		ClientToClientReflection: clientToClientReflection,
-//		Comment:                  bgp_comment,
-//		ConfederationPeers:       confederationPeers,
-//		Disabled:                 disabled,
-//		IgnoreAsPathLen:          ignoreAsPathLen,
-//		OutFilter:                outFilter,
-//		RedistributeConnected:    redistributeConnected,
-//		RedistributeOspf:         redistributeOspf,
-//		RedistributeOtherBgp:     redistributeOtherBgp,
-//		RedistributeRip:          redistributeRip,
-//		RedistributeStatic:       redistributeStatic,
-//		RouterId:                 routerId,
-//		RoutingTable:             routingTable,
-//		ClusterId:                noClusterId,
-//		Confederation:            noConfederation,
-//	}
-//	bgpInstance, err := c.AddBgpInstance(
-//		bgp_name,
-//		as,
-//		clientToClientReflection,
-//		bgp_comment,
-//		confederationPeers,
-//		disabled,
-//		ignoreAsPathLen,
-//		outFilter,
-//		redistributeConnected,
-//		redistributeOspf,
-//		redistributeOtherBgp,
-//		redistributeRip,
-//		redistributeStatic,
-//		routerId,
-//		routingTable,
-//		noClusterId,
-//		noConfederation,
-//	)
-//
-//	if err != nil {
-//		t.Fatalf("Error creating a bpg instance with: %v", err)
-//	}
-//	fmt.Println(bgpInstance)
-//	fmt.Println(expectedBgpInstance)
-//
-//	//if len(pool.Id) < 1 {
-//	//	t.Errorf("The created pool does not have an Id: %v", pool)
-//	//}
-//
-//	//if pool.Name != expectedPool.Name {
-//	//	t.Errorf("The pool Name fields do not match. actual: %v expected: %v", pool.Name, expectedPool.Name)
-//	//}
-//
-//	//if pool.Ranges != expectedPool.Ranges {
-//	//	t.Errorf("The pool Ranges fields do not match. actual: %v expected: %v", pool.Ranges, expectedPool.Ranges)
-//	//}
-//
-//	//if pool.Comment != expectedPool.Comment {
-//	//	t.Errorf("The pool Comment fields do not match. actual: %v expected: %v", pool.Comment, expectedPool.Comment)
-//	//}
-//
-//	//foundPool, err := c.FindPool(pool.Id)
-//
-//	//if err != nil {
-//	//	t.Errorf("Error getting pool with: %v", err)
-//	//}
-//
-//	//if !reflect.DeepEqual(pool, foundPool) {
-//	//	t.Errorf("Created pool and found pool do not match. actual: %v expected: %v", foundPool, pool)
-//	//}
-//
-//	//err = c.DeletePool(pool.Id)
-//
-//	//if err != nil {
-//	//	t.Errorf("Error deleting pool with: %v", err)
-//	//}
-//}
